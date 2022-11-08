00e2d79053a848a8bbded2db164658d780726f41
LOAN <lalix@gaming.tech>
2022-11-08 08:00:50 +0100

    description de ce que je veux push dans le repository

---
+++ algo.py
@@ -0,0 +1,74 @@
+
+r=12000
+s= 1250
+e=10
+rh=230 
+
+assertion1 = ((365 * 3) /(24 - (16-8))) * (rh) > r == (e * s < r)
+
+15740.625 > 12000 == 12500 < 12000
+true    ==       False
+false 
+
+assertion1pt1= (((365 * 3) /(24 - (16-8))) * (rh) > r) # true 
+assertion1pt2= (e * s < r) # false 
+
+#assertion1===(assertion1pt1==assertion1pt2)
+#assertion1===(assertion1pt1==false)
+#assertion1===(true==false)
+#assertion1===false
+
+print (assertion1)
+#PRINT assertion1
+#ECRIRE assertion1
+
+#camelCase 
+#kebab-case
+#PascalCase
+#snake_case
+ 
+assertionDEUX = ((365 * 3) /(4 - (12-8))) * (rh) > r == (e * s < r)
+
+assertionDEUXpt1= ((365 * 3) /(4 - (12-8))) * (rh) > r
+assertionDEUXpt2 = (e * s < r)
+
+#assertionDEUX===(assertionDEUXpt1==assertionDEUXpt2)
+#assertionDEUX===(false==assertionDEUXpt2)
+#assertionDEUX===(false==false)
+#assertionDEUX=== #false 
+
+def calculPtDeux(){
+       return ((e * s < r))
+}
+
+assertionDEUX = ((365 * 3) /(4 - (12-8))) * (rh) > r == def calculPtDeux(1,2,3)
+
+def add(x,y):
+    return(x+y)
+
+
+def sub(x,y):
+    return(x-y)
+
+
+def multiplite(x,y):
+    return(x*y)
+
+def divide(x,y):
+    return(x/y)
+
+def modulo(x,y):
+    return(x%y)
+
+
+
+
+#calculer le revenu par seconde 
+#salaireH
+#H/JO
+# année /JO  
+calculNET converti depuis brut 
+
+0.000360
+
+def 
