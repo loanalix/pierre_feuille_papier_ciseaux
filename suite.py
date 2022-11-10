a1e91e0cc19dd9546eac039212cc7149ff9eef26
loanalix <117281608+loanalix@users.noreply.github.com>
2022-11-10 11:34:03 +0100

    la suite fibonachi

---
+++ suite.py
@@ -0,0 +1,7 @@
+#definir u par n 
+#initialiser u comme egale a 1
+#afficher le u
+#Tanque la suite n'est pas arreter 
+    #Alors ajouter un-1 a u
+    #afficher le nouveau u 
+#retourner la fonction de la suite  
